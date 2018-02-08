d11ff66c632fc2d2cda78f8cd1c645877c64aa61
nicoliyun <24080559@qq.com>
2018-02-08 10:32:21 +0800

    test

---
+++ src/main/test/java/Hello.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class Hello {
+    public void test_m(int a,int b){
+        System.out.print(a/b);
+
+    }
+
+    @Test
+    public void test_o() {
+        test_m(4,2);
+    }
+}
